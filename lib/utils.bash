436d16f7d3684aaaf6d71f103cd56d92f958e335
Achilleas Pipinellis <axil@gitlab.com>
2023-07-06 11:47:31 +0200

    Run shfmt on utils.bash

--- lib/utils.bash
+++ lib/utils.bash
@@ -7,86 +7,86 @@ TOOL_NAME="minify"
 TOOL_TEST="minify --help"
 
 fail() {
-  echo -e "asdf-$TOOL_NAME: $*"
-  exit 1
+	echo -e "asdf-$TOOL_NAME: $*"
+	exit 1
 }
 
 get_os() {
-  os=$(uname -s)
-  case $os in
-  Darwin) os="darwin" ;;
-  Linux) os="linux" ;;
-  *) fail "The os (${os}) is not supported by this installation script." ;;
-  esac
-  echo "$os"
+	os=$(uname -s)
+	case $os in
+	Darwin) os="darwin" ;;
+	Linux) os="linux" ;;
+	*) fail "The os (${os}) is not supported by this installation script." ;;
+	esac
+	echo "$os"
 }
 
 get_arch() {
-  arch=$(uname -m)
-  case $arch in
-  x86_64) arch="amd64" ;;
-  arm64) arch="arm64" ;;
-  *) fail "The architecture (${arch}) is not supported by this installation script." ;;
-  esac
-  echo "$arch"
+	arch=$(uname -m)
+	case $arch in
+	x86_64) arch="amd64" ;;
+	arm64) arch="arm64" ;;
+	*) fail "The architecture (${arch}) is not supported by this installation script." ;;
+	esac
+	echo "$arch"
 }
 
 curl_opts=(-fsSL)
 
 if [ -n "${GITHUB_API_TOKEN:-}" ]; then
-  curl_opts=("${curl_opts[@]}" -H "Authorization: token $GITHUB_API_TOKEN")
+	curl_opts=("${curl_opts[@]}" -H "Authorization: token $GITHUB_API_TOKEN")
 fi
 
 sort_versions() {
-  sed 'h; s/[+-]/./g; s/.p\([[:digit:]]\)/.z\1/; s/$/.z/; G; s/\n/ /' |
-    LC_ALL=C sort -t. -k 1,1 -k 2,2n -k 3,3n -k 4,4n -k 5,5n | awk '{print $2}'
+	sed 'h; s/[+-]/./g; s/.p\([[:digit:]]\)/.z\1/; s/$/.z/; G; s/\n/ /' |
+		LC_ALL=C sort -t. -k 1,1 -k 2,2n -k 3,3n -k 4,4n -k 5,5n | awk '{print $2}'
 }
 
 list_github_tags() {
-  git ls-remote --tags --refs "$GH_REPO" |
-    grep -o 'refs/tags/.*' | cut -d/ -f3- |
-    sed 's/^v//'
+	git ls-remote --tags --refs "$GH_REPO" |
+		grep -o 'refs/tags/.*' | cut -d/ -f3- |
+		sed 's/^v//'
 }
 
 list_all_versions() {
-  # Change this function if minify has other means of determining installable versions.
-  list_github_tags
+	# Change this function if minify has other means of determining installable versions.
+	list_github_tags
 }
 
 download_release() {
-  local version filename url
-  version="$1"
-  filename="$2"
+	local version filename url
+	version="$1"
+	filename="$2"
 
-  arch=$(get_arch)
-  os=$(get_os)
-  url="$GH_REPO/releases/download/v${version}/minify_${os}_${arch}.tar.gz"
+	arch=$(get_arch)
+	os=$(get_os)
+	url="$GH_REPO/releases/download/v${version}/minify_${os}_${arch}.tar.gz"
 
-  echo "* Downloading $TOOL_NAME release $version..."
-  curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
+	echo "* Downloading $TOOL_NAME release $version..."
+	curl "${curl_opts[@]}" -o "$filename" -C - "$url" || fail "Could not download $url"
 }
 
 install_version() {
-  local install_type="$1"
-  local version="$2"
-  local install_path="${3%/bin}/bin"
+	local install_type="$1"
+	local version="$2"
+	local install_path="${3%/bin}/bin"
 
-  if [ "$install_type" != "version" ]; then
-    fail "asdf-$TOOL_NAME supports release installs only"
-  fi
+	if [ "$install_type" != "version" ]; then
+		fail "asdf-$TOOL_NAME supports release installs only"
+	fi
 
-  (
-    local tool_cmd
-    tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
+	(
+		local tool_cmd
+		tool_cmd="$(echo "$TOOL_TEST" | cut -d' ' -f1)"
 
-    mkdir -p "$install_path"
-    cp -r "$ASDF_DOWNLOAD_PATH/${tool_cmd}" "$install_path/$tool_cmd"
+		mkdir -p "$install_path"
+		cp -r "$ASDF_DOWNLOAD_PATH/${tool_cmd}" "$install_path/$tool_cmd"
 
-    test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
+		test -x "$install_path/$tool_cmd" || fail "Expected $install_path/$tool_cmd to be executable."
 
-    echo "$TOOL_NAME $version installation was successful!"
-  ) || (
-    rm -rf "$install_path"
-    fail "An error occurred while installing $TOOL_NAME $version."
-  )
+		echo "$TOOL_NAME $version installation was successful!"
+	) || (
+		rm -rf "$install_path"
+		fail "An error occurred while installing $TOOL_NAME $version."
+	)
 }
